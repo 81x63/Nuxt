bda2ed0dafa2876d07a8d03add7731e8b1ae048a
Takuya <104154368+tak-hz@users.noreply.github.com>
2023-02-14 06:36:45 +0000

    first

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="container">
-    <h1>Hello World</h1>
-  </div>
+
+    <footer>
+      <div class="footer-top">
+        <div class="container">
+          <div class="row gy-4">
+            <div class="col-md-4">
+              <h4 class="text-white">過去と未来</h4>
+            </div>
+            <div class="col-md-2">
+              <h5 class="text-white">Menu</h5>
+              <ul class="list-unstyled">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <h5 class="text-white">Address</h5>
+              <ul class="list-unstyled">
+                <li>Address: , FL 62626</li>
+                <li>Email: XXXXXX@icloud.com</li>
+                <li>Phone: 000-000-000</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 </template>
